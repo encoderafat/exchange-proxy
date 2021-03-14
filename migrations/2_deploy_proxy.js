79948377d52dd95f4350870e0cf585fe8cc9c7a2
Rafat Hussain <rafat.hsn@gmail.com>
2021-03-14 18:55:34 +0530

    contracts deployed

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -2,7 +2,7 @@ const ExchangeProxy = artifacts.require("ExchangeProxy");
 const WETH9 = artifacts.require("WETH9");
 
 module.exports = async function(deployer, network, accounts) {
-    if (network == 'development' || network == 'coverage') {
+    if (network == 'development' || network == 'coverage' || network == 'matic') {
         await deployer.deploy(WETH9);
         await deployer.deploy(ExchangeProxy, WETH9.address);
     } else if (network == 'kovan-fork' || network == 'kovan') {
